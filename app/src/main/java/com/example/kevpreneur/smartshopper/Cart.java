b6e92d7494aa01709505c2bdfedaf054417cd27e
KevPreneur <1kevmosh@gmail.com>
2018-05-12 22:15:52 +0400

    added the cart
    added the cart with sample products

---
+++ app/src/main/java/com/example/kevpreneur/smartshopper/Cart.java
@@ -0,0 +1,87 @@
+package com.example.kevpreneur.smartshopper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class Cart extends AppCompatActivity {
+
+    String[] productArray = {"Apples (pack of 5)	- MUR 80.94",
+            "Bananas (1kg)	- MUR 50.68",
+            "Oranges (pack of 5)	- MUR 11.50",
+            "Pineapple	- MUR 50.69",
+            "Mango	- MUR 70.75",
+            "Potatoes (2.5kg)	- MUR 11.98",
+            "Tomatoes (1kg)	- MUR 21.99",
+            "Onions (pack of 4)	- MUR 51.83",
+            "Lettuce	- MUR 47.49",
+            "Rice (1kg)	- MUR 40.48",
+            "Pasta (1kg)	- MUR 80.00",
+            "Bread (800g loaf)	- MUR 15.00",
+            "Pizza (350g)	- MUR 40.60",
+            "Beef Mince (500g)	- MUR 75.99",
+            "Chicken Breast (pack of 2)	- MUR 78.00",
+            "Salmon Fillets (pack of 2)	- MUR 500.23",
+            "Pasta Sauce (500g jar)	- MUR 41.84",
+            "Curry Sauce (500g jar)	- MUR 20.84",
+            "Cheese (250g)	- MUR 23.74",
+            "Butter (250g)	- MUR 20.92",
+            "Plain Yoghurt (500g)	- MUR 60.94",
+            "Milk (568ml / 1pint)	- MUR 20.58",
+            "Milk (2.27L / 4pints)	- MUR 150.89",
+            "Fresh Orange Juice (1L)	- MUR 20.69",
+            "Cola (2L)	- MUR 25.00",
+            "Beer (pack of 4 bottles)	- MUR 410.00"};
+//    ArrayList<String> cartItems = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+//        cartItems.add("Colgate");
+//        cartItems.add("Shoe");
+//        cartItems.add("Laptop");
+//        cartItems.add("Mouse");
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_listview,productArray);
+
+        ListView listView  = (ListView) findViewById(R.id.items_List);
+        listView.setAdapter(adapter);
+
+        findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Cart.this, ScanActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_checkout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Cart.this, ScanActivity.class));
+            }
+        });
+    }
+
+//
+//    public void addToCart (ProductDetails.Product product){
+////        repository.add(product);
+//    }
+//
+//    public void removeFromCart (ProductDetails.Product product){
+////        repository.remove(product);
+//    }
+//
+//    private void getRepoItems(){
+//        for (ProductDetacccccccccccccccils.Product product : repository ){
+//            cartItems.add(product.getName());
+//        }
+//    }
+
+}
--- app/src/main/java/com/example/kevpreneur/smartshopper/MainActivity.java
+++ app/src/main/java/com/example/kevpreneur/smartshopper/MainActivity.java
@@ -25,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ScanActivity.class));
             }
         });
+
+        findViewById(R.id.view_cart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Cart.class));
+            }
+        });
     }
 }
